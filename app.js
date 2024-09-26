feda3189c8146a81e688143a5c3471ef2a62a869
Steve Mwangi <mwangisteve.irungu@gmail.com>
2024-09-26 13:27:28 +0300

    prototype

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classlist.add('show');
-        } else{
-            entry.target.classlist.remove('show');
-        }
-    });
-});
+function reveal(){
+    // change to a position where the effect will be aplied
+    const offset = 100
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    document.querySelectorAll(".reveal").forEach(item => {
+               
+        if (item.getBoundingClientRect().top - window.innerHeight < -(offset)){
+            item.classList.add("active")
+        }
+    })
+
+    //when scrolling back and reaching the beginning of the page every object revealed will be hidden again in order to start the animation again
+    if (window.scrollY==0){
+        document.querySelectorAll(".reveal").forEach(item => {
+            item.classList.remove("active")
+        })
+    }
+}
+
+//adding the function on the scroll event of the browser
+window.addEventListener("scroll", reveal)
+
+const swiper = new Swiper('.swiper', {
+    loop: true, 
+    slidesPerView: "auto",
+    centeredSlides: true,
+    observeParents: !0,
+    observer: !0
+  });
